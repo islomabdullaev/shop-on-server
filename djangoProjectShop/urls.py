0174417b8188d1ce0089d5a719e4a7e79a166c85
Islom <islom.abdullaev.02@mail.ru>
2022-03-09 17:14:19 +0500

    asdsa

--- djangoProjectShop/urls.py
+++ djangoProjectShop/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = i18n_patterns(
     path("accounts/", include("accounts.urls", namespace="accounts")),
@@ -11,7 +12,8 @@ urlpatterns = i18n_patterns(
     path("products/", include("products.urls", namespace="products")),
     path("orders/", include("orders.urls", namespace="orders")),
     path("users/", include("users.urls", namespace="users")),
-    path("", include("pages.urls", namespace="pages"))
+    path("", include("pages.urls", namespace="pages")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 )
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
